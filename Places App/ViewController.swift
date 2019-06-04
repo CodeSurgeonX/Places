8e918db6dea18b420d7e77fc97bc4521061a0bdc
Shashwat Kashyap <shashwat.kashyap2@gmail.com>
2019-06-04 23:14:47 +0530

    Initial Commit

---
+++ Places App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places App
+//
+//  Created by Shashwat  on 04/06/19.
+//  Copyright © 2019 Shashwat . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
